cf2ce95fed1dc09a5a7cccc9b380418a3ad2eb46
panchanan16 <dekapanchanan16@gmail.com>
2025-05-21 14:29:32 +0530

    about us corrected

--- src/app/(user)/about-us/page.js
+++ src/app/(user)/about-us/page.js
@@ -141,13 +141,13 @@ async function AboutUsPage() {
             {/* Custom CSS to style the rendered HTML */}
             <StyleSheet />
             <div className="bg-white shadow-sm rounded-lg p-8">
-                <h1 className="text-3xl font-bold text-gray-800 mb-6 pb-2 border-b border-gray-200">{html.data[0].title}</h1>
+                <h1 className="text-3xl font-bold text-gray-800 mb-6 pb-2 border-b border-gray-200">{html[0].title}</h1>
 
                 <section className="mb-10">
                     {/* Using dangerouslySetInnerHTML to render the HTML content */}
                     <div
                         className="html-content"
-                        dangerouslySetInnerHTML={{ __html: html.data[0].content }}
+                        dangerouslySetInnerHTML={{ __html: html[0].content }}
                     />
                 </section>
             </div>
