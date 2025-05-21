f0f1972c085dead86ed0ba25f1b50378f313ce2e
panchanan16 <dekapanchanan16@gmail.com>
2025-05-21 14:06:16 +0530

    config added

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,3 +1,6 @@
-export const entityHead = `http://localhost:3100/api/v1/entity`
-export const entityCore = `http://localhost:3100/api/v1/core`
+// export const entityHead = `http://localhost:3100/api/v1/entity`
+// export const entityCore = `http://localhost:3100/api/v1/core`
+
+export const entityHead = `http://89.116.230.122:3100/api/v1/entity`
+export const entityCore = `http://89.116.230.122:3100/api/v1/core`
 
