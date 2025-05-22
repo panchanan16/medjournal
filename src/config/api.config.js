65348aaaa1d924d43877bdd598d19e47953ebe42
panchanan16 <dekapanchanan16@gmail.com>
2025-05-22 22:32:54 +0530

    config added

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,6 +1,6 @@
-// export const BASE_URL = `http://89.116.230.122:3100`
+export const BASE_URL = `http://89.116.230.122:3100`
 
-export const BASE_URL = `http://localhost:3100`
+// export const BASE_URL = `http://localhost:3100`
 
 export const entityHead = `${BASE_URL}/api/v1/entity`
 export const entityCore = `${BASE_URL}/api/v1/core`
