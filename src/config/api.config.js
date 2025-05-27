5eaa732189044ce2065b715d9cee2695017ab314
panchanan16 <dekapanchanan16@gmail.com>
2025-05-28 01:08:36 +0530

    config changes

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,4 +1,8 @@
-export const BASE_URL = `https://app.farmclin.es`
+// export const BASE_URL = `https://app.farmclin.es`
+
+export const BASE_URL = `http://194.164.150.106:3100`
+
+
 
 // export const BASE_URL = `http://localhost:3100`
 
